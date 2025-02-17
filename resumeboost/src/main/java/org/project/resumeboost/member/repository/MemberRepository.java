58d2a9d7a2a2b94aae83b927f25bf652327ff564
bebest90 <bebest90@daum.net>
2025-02-17 15:00:05 +0900

    item

--- resumeboost/src/main/java/org/project/resumeboost/member/repository/MemberRepository.java
+++ resumeboost/src/main/java/org/project/resumeboost/member/repository/MemberRepository.java
@@ -2,8 +2,9 @@ package org.project.resumeboost.member.repository;
 
 import org.project.resumeboost.member.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface MemberRepository extends JpaRepository<MemberEntity,Long>{
+@Repository
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
 
-  
 }
