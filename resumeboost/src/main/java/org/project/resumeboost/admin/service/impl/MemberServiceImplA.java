7747a352a27c5e8422be77fe8dfeaab3bbb2c81e
hws-13151 <hws34810@gmail.com>
2025-03-07 12:56:04 +0900

    push

--- resumeboost/src/main/java/org/project/resumeboost/admin/service/impl/MemberServiceImplA.java
+++ resumeboost/src/main/java/org/project/resumeboost/admin/service/impl/MemberServiceImplA.java
@@ -31,60 +31,55 @@ public class MemberServiceImplA implements MemberServiceA {
   private final MemberImgRepository memberImgRepository;
 
   @Value("${file.path}")
-	String saveFile;
+  String saveFile;
 
   @Override
   public Page<MemberDto> ListAll(Pageable pageable) {
-    
+
     Page<MemberEntity> memberEntities = memberRepository.findAll(pageable);
-    
-    return (
-      memberEntities.map(el -> MemberDto.toMemberDto(el))
-    );
+
+    return (memberEntities.map(el -> MemberDto.toMemberDto(el)));
 
   }
 
-
   @Override
   public MemberDto memberDetail(Long id) {
-    
-    MemberEntity memberEntity = memberRepository.findById(id).orElseThrow(() -> new NullPointerException("회원이 존재하지 않습니다!"));
-   
-  
+
+    MemberEntity memberEntity = memberRepository.findById(id)
+        .orElseThrow(() -> new NullPointerException("회원이 존재하지 않습니다!"));
+
     return MemberDto.toMemberDto(memberEntity);
 
   }
 
-
   @Override
   public void memberDelete(Long id) {
-    
+
     Optional<MemberEntity> optionalMemberEntity = memberRepository.findById(id);
     if (!optionalMemberEntity.isPresent()) {
       throw new NullPointerException("회원이 존재하지 않습니다!!");
     }
-    
 
     memberRepository.deleteById(id);
-    
+
   }
 
   @Override
   public void memberUpdate(MemberDto memberDto) throws IllegalStateException, IOException {
-    
+
     Optional<MemberEntity> optionalMemberEntity = memberRepository.findById(memberDto.getId());
     if (!optionalMemberEntity.isPresent()) {
       throw new NullPointerException("수정할 회원이 존재하지 않습니다!!");
     }
 
+    Optional<MemberImgEntity> optionalMemberImgEntity = memberImgRepository
+        .findByMemberEntity(MemberEntity.builder().id(memberDto.getId()).build());
 
-    Optional<MemberImgEntity> optionalMemberImgEntity = memberImgRepository.findByMemberEntity(MemberEntity.builder().id(memberDto.getId()).build());
-    
     if (optionalMemberImgEntity.isPresent()) {
       String newImgName = optionalMemberImgEntity.get().getNewImgName();
       String saveFilePath = saveFile + "/member/profile/" + newImgName;
 
-      File deleteFile = new File(saveFilePath); // 이미 있으면 삭제 
+      File deleteFile = new File(saveFilePath); // 이미 있으면 삭제
 
       if (deleteFile.exists()) {
         deleteFile.delete();
@@ -97,40 +92,37 @@ public class MemberServiceImplA implements MemberServiceA {
       memberImgRepository.deleteById(optionalMemberImgEntity.get().getId());
 
     }
-    
-    
+
     if (memberDto.getProfileFile() == null) {
 
       memberRepository.save(MemberEntity.builder()
-        .id(memberDto.getId())
-        .userEmail(memberDto.getUserEmail())
-        .userPw(memberDto.getUserPw())
-        .age(memberDto.getAge())
-        .address(memberDto.getAddress())
-        .nickName(memberDto.getNickName())
-        .userName(memberDto.getUserName())
-        .detailTitle(memberDto.getDetailTitle())
-        .detail(memberDto.getDetail())
-        .myReplyCount(memberDto.getMyReplyCount())
-        .viewCount(memberDto.getViewCount())
-        .myReplyCount(memberDto.getMyReplyCount())
-        .myPostCount(memberDto.getMyPostCount())
-        .career(memberDto.getCareer())
-        .role(memberDto.getRole())
-        .attachFile(memberDto.getAttachFile())
-        .portfolioFile(memberDto.getPortfolioFile())
-        .phone(memberDto.getPhone())
-        .boardEntities(memberDto.getBoardEntities())
-        .replyEntities(memberDto.getReplyEntities())
-        .itemEntities(memberDto.getItemEntities())
-        .payEntities(memberDto.getPayEntities())
-        .memberImgEntities(memberDto.getMemberImgEntities())
-        .memberPtEntities(memberDto.getMemberPtEntities())
-        .build());
+          .id(memberDto.getId())
+          .userEmail(memberDto.getUserEmail())
+          .userPw(memberDto.getUserPw())
+          .age(memberDto.getAge())
+          .address(memberDto.getAddress())
+          .nickName(memberDto.getNickName())
+          .userName(memberDto.getUserName())
+          .detail(memberDto.getDetail())
+          .myReplyCount(memberDto.getMyReplyCount())
+          .viewCount(memberDto.getViewCount())
+          .myReplyCount(memberDto.getMyReplyCount())
+          .myPostCount(memberDto.getMyPostCount())
+          .career(memberDto.getCareer())
+          .role(memberDto.getRole())
+          .attachFile(memberDto.getAttachFile())
+          .portfolioFile(memberDto.getPortfolioFile())
+          .phone(memberDto.getPhone())
+          .boardEntities(memberDto.getBoardEntities())
+          .replyEntities(memberDto.getReplyEntities())
+          .itemEntities(memberDto.getItemEntities())
+          .payEntities(memberDto.getPayEntities())
+          .memberImgEntities(memberDto.getMemberImgEntities())
+          .memberPtEntities(memberDto.getMemberPtEntities())
+          .build());
     } else {
       MultipartFile memberImgFile = memberDto.getProfileFile();
       UUID uuid = UUID.randomUUID();
-
 
       String oldImgName = memberImgFile.getOriginalFilename();
       String newImgName = uuid + oldImgName;
@@ -138,55 +130,42 @@ public class MemberServiceImplA implements MemberServiceA {
       String saveFilePath = saveFile + "/member/profile/" + newImgName;
       memberImgFile.transferTo(new File(saveFilePath));
 
-
       memberRepository.save(MemberEntity.builder()
-        .id(memberDto.getId())
-        .userEmail(memberDto.getUserEmail())
-        .userPw(memberDto.getUserPw())
-        .age(memberDto.getAge())
-        .address(memberDto.getAddress())
-        .nickName(memberDto.getNickName())
-        .userName(memberDto.getUserName())
-        .detailTitle(memberDto.getDetailTitle())
-        .detail(memberDto.getDetail())
-        .myReplyCount(memberDto.getMyReplyCount())
-        .viewCount(memberDto.getViewCount())
-        .myReplyCount(memberDto.getMyReplyCount())
-        .myPostCount(memberDto.getMyPostCount())
-        .career(memberDto.getCareer())
-        .role(memberDto.getRole())
-        .attachFile(1)
-        .portfolioFile(memberDto.getPortfolioFile())
-        .phone(memberDto.getPhone())
-        .boardEntities(memberDto.getBoardEntities())
-        .replyEntities(memberDto.getReplyEntities())
-        .itemEntities(memberDto.getItemEntities())
-        .payEntities(memberDto.getPayEntities())
-        .memberImgEntities(memberDto.getMemberImgEntities())
-        .memberPtEntities(memberDto.getMemberPtEntities())
-        .build());
-      
-        
+          .id(memberDto.getId())
+          .userEmail(memberDto.getUserEmail())
+          .userPw(memberDto.getUserPw())
+          .age(memberDto.getAge())
+          .address(memberDto.getAddress())
+          .nickName(memberDto.getNickName())
+          .userName(memberDto.getUserName())
+          .detail(memberDto.getDetail())
+          .myReplyCount(memberDto.getMyReplyCount())
+          .viewCount(memberDto.getViewCount())
+          .myReplyCount(memberDto.getMyReplyCount())
+          .myPostCount(memberDto.getMyPostCount())
+          .career(memberDto.getCareer())
+          .role(memberDto.getRole())
+          .attachFile(1)
+          .portfolioFile(memberDto.getPortfolioFile())
+          .phone(memberDto.getPhone())
+          .boardEntities(memberDto.getBoardEntities())
+          .replyEntities(memberDto.getReplyEntities())
+          .itemEntities(memberDto.getItemEntities())
+          .payEntities(memberDto.getPayEntities())
+          .memberImgEntities(memberDto.getMemberImgEntities())
+          .memberPtEntities(memberDto.getMemberPtEntities())
+          .build());
 
-        MemberImgEntity memberImgEntity = MemberImgEntity.builder()
-        .newImgName(newImgName)
-        .oldImgName(oldImgName)
-        .memberEntity(optionalMemberEntity.get())
-        .build();
+      MemberImgEntity memberImgEntity = MemberImgEntity.builder()
+          .newImgName(newImgName)
+          .oldImgName(oldImgName)
+          .memberEntity(optionalMemberEntity.get())
+          .build();
 
-        memberImgRepository.save(memberImgEntity);
+      memberImgRepository.save(memberImgEntity);
 
     }
 
-
-
-
-
-
   }
 
-
-  
 }
-
-
