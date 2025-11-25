40a2f2a597f415708f0f0e8202563ae0c6f2c0b2
hrdtjava <hrdtjava@gmail.com>
2025-11-25 12:28:47 +0900

    오픈API work24

--- resumeboost/src/main/java/org/project/resumeboost/openApi/service/impl/Work24ApiServiceImpl.java
+++ resumeboost/src/main/java/org/project/resumeboost/openApi/service/impl/Work24ApiServiceImpl.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.project.resumeboost.openApi.service.Work24ApiService;
@@ -14,25 +15,27 @@ import org.springframework.http.ResponseEntity;
 @RequiredArgsConstructor
 public class Work24ApiServiceImpl implements Work24ApiService {
 
-  private static final String BASE_URL = "https://www.work24.go.kr/cm/openApi/call/wk/callOpenApiSvcInfo210L21.do";
-  private static final String AUTH_KEY = "";
+    private static final String BASE_URL = "https://www.work24.go.kr/cm/openApi/call/wk/callOpenApiSvcInfo210L21.do";
 
-  private final RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
-  @Override
-  public JsonNode getWork24Data(int page, int display) {
-    String url = String.format("%s?authKey=%s&callTp=L&returnType=XML&startPage=%d&display=%d",
-        BASE_URL, AUTH_KEY, page, display);
+    @Value("${openapi.work24.auth-key}")
+    private String authKey;  // YAML에서 주입받는 방식
 
-    ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-    String xmlData = response.getBody();
+    @Override
+    public JsonNode getWork24Data(int page, int display) {
+        String url = String.format("%s?authKey=%s&callTp=L&returnType=XML&startPage=%d&display=%d",
+                BASE_URL, authKey, page, display);
 
-    try {
-      // XML을 JSON으로 변환
-      XmlMapper xmlMapper = new XmlMapper();
-      return xmlMapper.readTree(xmlData);
-    } catch (Exception e) {
-      throw new RuntimeException("XML to JSON 변환 중 오류 발생", e);
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        String xmlData = response.getBody();
+
+        try {
+            // XML을 JSON으로 변환
+            XmlMapper xmlMapper = new XmlMapper();
+            return xmlMapper.readTree(xmlData);
+        } catch (Exception e) {
+            throw new RuntimeException("XML to JSON 변환 중 오류 발생", e);
+        }
     }
-  }
 }
